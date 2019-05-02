378b373e07852157fae6be4e484348c5d42f6030
afdhal kurniawan <afdhal.kurniawan@ui.ac.id>
2019-05-02 17:57:57 +0700

    CS_modul9

---
+++ app/src/main/java/com/example/jstore_android_afdhalkurniawan/layout_supplier.java
@@ -0,0 +1,13 @@
+package com.example.jstore_android_afdhalkurniawan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout_supplier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_supplier);
+    }
+}
